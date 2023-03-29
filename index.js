7026644063c8390c2918c01f3a806ea6d836c34a
Cindy Velasquez <ciindvlz@hotmail.com>
2023-03-29 19:11:12 -0400

    FuncCon

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun (act = 'roller-skate'){
+    return `This Saturday, I want to ${act}!`
+};
+function mondayWork (day = 'go to the office'){
+    return `This Monday, I will ${day}.`
+};
+function wrapAdjective (low="*"){
+    return function (high){
+        return `You are ${low}${high}${low}!`
+    }
+}
+console.log(wrapAdjective('||'));
